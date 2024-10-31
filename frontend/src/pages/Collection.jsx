116fa7427f87049d2fdee806c46a605f1ca639df
dursunaydin <dursunayydin@gmail.com>
2024-10-31 11:35:48 +0300

    Product Sort

--- frontend/src/pages/Collection.jsx
+++ frontend/src/pages/Collection.jsx
@@ -9,6 +9,7 @@ const Collection = () => {
   const [filterProducts, setFilterProducts] = useState([]);
   const [category, setCategory] = useState([]);
   const [subCategory, setSubCategory] = useState([]);
+  const [sortType, setSortType] = useState("relavent");
 
   const toogleCategory = (e) => {
     if (category.includes(e.target.value)) {
@@ -43,6 +44,28 @@ const Collection = () => {
 
     setFilterProducts(productsCopy);
   };
+
+  const sortProduct = () => {
+    let fpCopy = filterProducts.slice();
+
+    switch (sortType) {
+      case "low-high":
+        setFilterProducts(fpCopy.sort((a, b) => a.price - b.price));
+        break;
+
+      case "high-low":
+        setFilterProducts(fpCopy.sort((a, b) => b.price - a.price));
+        break;
+
+      default:
+        applyFilter();
+        break;
+    }
+  };
+
+  useEffect(() => {
+    sortProduct();
+  }, [sortType]);
 
   useEffect(() => {
     applyFilter();
@@ -145,8 +168,13 @@ const Collection = () => {
         <div className="flex justify-between text-base sm:text-2xl mb-4">
           <Title text1="ALL" text2="COLLECTIONS" />
           {/* Products Sort */}
-          <select className="border-2 border-gray-300 text-sm px-2">
-            <option value="relavent">Sort by: </option>
+          <select
+            onChange={(e) => setSortType(e.target.value)}
+            className="border-2 border-gray-300 text-sm px-2"
+          >
+            <option value="relavent">Sort by: Relavent </option>
+            <option value="low-high">Sort by: Low to High</option>
+            <option value="high-low">Sort by: High to Low</option>
           </select>
         </div>
 
