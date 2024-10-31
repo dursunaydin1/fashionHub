9abd748690682dda9dd5269637ba639d0bb5f4eb
dursunaydin <dursunayydin@gmail.com>
2024-10-31 14:00:51 +0300

    Create Add to cart feature

--- frontend/src/pages/Product.jsx
+++ frontend/src/pages/Product.jsx
@@ -8,7 +8,7 @@ const Product = () => {
   const [productData, setProductData] = useState(false);
   const [image, setImage] = useState("");
   const [size, setSize] = useState("");
-  const { products, currency } = useContext(ShopContext);
+  const { products, currency, addToCart } = useContext(ShopContext);
 
   const fetchProduct = async () => {
     products.map((item) => {
@@ -79,7 +79,10 @@ const Product = () => {
               ))}
             </div>
           </div>
-          <button className="bg-black text-white px-8 py-3 text-sm active:bg-gray-700">
+          <button
+            onClick={() => addToCart(productData._id, size)}
+            className="bg-black text-white px-8 py-3 text-sm active:bg-gray-700"
+          >
             ADD TO CART
           </button>
           <hr className="mt-8 sm:w-4/5" />
