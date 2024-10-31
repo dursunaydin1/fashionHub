6116fd008adcf336eec94464048e421c0f00e419
dursunaydin <dursunayydin@gmail.com>
2024-10-31 18:45:49 +0300

    Added MongoDB Configuration

---
+++ backend/server.js
@@ -0,0 +1,22 @@
+import express from "express";
+import cors from "cors";
+import "dotenv/config";
+import connectDB from "./config/mongodb.js";
+
+// App Config
+const app = express();
+const port = process.env.PORT || 4000;
+connectDB();
+
+// Middleware
+app.use(express.json());
+app.use(cors());
+
+// api endpoints
+app.get("/", (req, res) => {
+  res.send("API is working");
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on port: ${port}`);
+});
